20744d8f4f76fb7107ad1cd78729bf4f5b3eaf93
JunyupKim <ytrewq271828@naver.com>
2025-08-08 17:01:30 +0900

    Default kernel with Gaussian noise via WhiteKernel

--- analysis.py
+++ analysis.py
@@ -3,32 +3,39 @@ import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import ConstantKernel, RBF, WhiteKernel
 from mpl_toolkits.mplot3d import Axes3D
 from numpy.typing import NDArray
+from scipy.interpolate import interp1d
 from typing import Tuple, Optional
+from gpal.utils import BoundsType
 
-def plotStd(figsize: Tuple[int, int], opt:str, dir:str, sbjID:int, x_pred:NDArray, trial_idx:int):
+def plotStdFitGPAL(figsize: Tuple[int, int], constant_value:float, cv_range:BoundsType,
+                   length_scale:float, ls_range:BoundsType, noise_level:float, nl_range:BoundsType,
+                   dir:str, sbjID:int, x_pred:NDArray, trial_idx:int):
     filePath=os.path.join(dir, f"{opt}_results_{sbjID}.csv")
     if os.path.exists(filePath):
         df=pd.read_csv(filePath)
     else:
-        raise FileNotFoundError(f"The following file is not found: {filePath}")
+        raise FileNotFoundError(f"The following file cannot not found: {filePath}")
     
     gns=df['given_number'].to_numpy()
     ests=df['estimation'].to_numpy()
-    mean=df['posterior_mean'].to_numpy()
-    std=df['posterior_std'].to_numpy()
 
-    meanArrPath=os.path.join(dir, f"{opt}_posterior_mean_{sbjID}.csv")
-    if os.path.exists(meanArrPath):
-        meanArr=np.loadtxt(meanArrPath, delimiter=',')
-    stdArrPath=os.path.join(dir, f"{opt}_posterior_std_{sbjID}.csv")
-    if os.path.exists(stdArrPath):
-        stdArr=np.loadtxt(stdArrPath, delimiter=',')
+    kernel=ConstantKernel(constant_value, cv_range)*RBF(length_scale, ls_range)+WhiteKernel(noise_level, nl_range)
+    #kernel=RBF(length_scale, ls_range)+WhiteKernel(noise_level, nl_range)
+    gpr=GaussianProcessRegressor(kernel, normalize_y=True, n_restarts_optimizer=100)
+
+    for ti in range(1,len(gns)):
+        gpr.fit(np.expand_dims(gns[:ti], -1), ests[:ti])
     
-    #plt.subplot(1,2,1)
-    plotStd1D(figsize, gns[:trial_idx], x_pred, ests[:trial_idx], meanArr[trial_idx], stdArr[trial_idx],
-               "Given Number", "Estimate", f"Posterior mean and std: Subject #{sbjID}, Trial #0 - #{trial_idx}, Optimization:{opt}", sigma_coef=1.0)
+    print(f"get_params: {gpr.kernel_.get_params()}")
+    #print(f"length_scale: {np.exp(gpr.kernel_.k2.theta).item()}")
+    post_mean, post_std = gpr.predict(np.expand_dims(x_pred, -1), return_std=True)
+
+    plotStd1D(figsize, gns, x_pred, ests, post_mean, post_std,
+               "Given Number", "Estimate", f"Subject #{sbjID}, Trial #0 - #{trial_idx}", sigma_coef=1.0)
     
     #plt.subplot(1,2,2)
     #plotEstim1D(figsize, gns, ests, "Given Number", "Estimate", 
@@ -52,13 +59,58 @@ def plotFreq(figsize:Tuple[int, int], dir:str, opt:str, n_trials: int, bin:int, 
     plotFreq1D(figsize, N, gns, bin, ranges, mode='average', xlabel="Given Number (Optimized)", title=f"Design selection frequencies ({mode})")
 
 
+def compare3(figsize:Tuple[int, int], dir:str, sbjID:int, x_pred:NDArray):
+    opts=['gpal', 'ado', 'random']
+    colors=['orange', 'black', 'green']
+    plt.figure()
+    for i, opt in enumerate(opts):
+        if i==0:
+            meanPath=os.path.join(dir, f"{opt}_posterior_mean_{sbjID}.csv")
+            if os.path.exists(meanPath):
+                meanArr=np.loadtxt(meanPath, delimiter=',')
+            else:
+                raise FileNotFoundError(f"The following file does not exist: {meanPath}")
+            stdPath=os.path.join(dir, f"{opt}_posterior_std_{sbjID}.csv")
+            if os.path.exists(stdPath):
+                stdArr=np.loadtxt(stdPath, delimiter=',')
+        else:
+            raise FileNotFoundError(f"The following file cannot be found: {filePath}")
+        gns=df['given_number'].to_numpy()
+        if i==0:
+            estMean=df['posterior_mean'].to_numpy()
+        argIdx=np.argsort(gns)
+        gns=gns[argIdx]
+        estMean=estMean[argIdx]
 
+        N=len(gns)
+        idx=np.arange(N)
+        gns_x=interp1d(idx, gns, kind='slinear')
+        est_y=interp1d(idx, est, kind='slinear')
+
+        idx_interp=np.linspace(0, N-1, 10*(N-1))
+        gns_interp=gns_x(idx_interp)
+        est_interp=est_y(idx_interp)
+
+        plt.plot(gns_interp, est_interp, color=colors[i])
+    plt.xlabel("Given Number")
+    plt.ylabel("Estimates")
+
+        
 if __name__=="__main__":
-    figsize=(16,8)
+    figsize=(12,8)
     n_trials=20
     x_pred=np.linspace(5, 500, (500-5)//5+1)
-    sbjID=25080810
+    sbjID=25080815
     opt='gpal'
     dir=os.path.join('experiment_results', f'participant_{sbjID}')
-    plotStd(figsize, opt, dir, sbjID, x_pred, trial_idx=19)
+    #dir='results'
+
+    constant_value=1.0
+    cv_range=(1e-5, 1e5)
+    length_scale=100.0
+    ls_range=(1e-5, 1e5)
+    noise_level=0.05
+    nl_range=(1e-5, 1e1)
+    plotStdFitGPAL(figsize, constant_value, cv_range, length_scale, ls_range, noise_level, nl_range, 
+                   dir, sbjID, x_pred, trial_idx=n_trials-1)
     #plotFreq(figsize, opt, dir, n_trials, bin=10, ranges=(0.0, 500.0), mode='sum')#
