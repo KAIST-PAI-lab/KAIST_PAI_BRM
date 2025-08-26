bbb5d0e2ca71848aca4672fa3d7e6f8fd5b33deb
Chaeho Lee <ch.chell39@gmail.com>
2025-08-26 20:30:31 +0900

    Add 2CPM to the result analysis

--- result_analysis_BIC_comparison.py
+++ result_analysis_BIC_comparison.py
@@ -60,12 +60,12 @@ for folder in data_dir.iterdir():
 
                     # Optimize MLLM
                     # MLLM params
-                    model_param_names = list(
+                    MLLM_model_param_names = list(
                         SIM_CONFIG["parameter_lists"]["params_mixed_log_linear"].keys()
                     )
                     # x0 of MLLM
-                    x0 = []
-                    for name in model_param_names:
+                    x0_MLLM = []
+                    for name in MLLM_model_param_names:
                         start_value = SIM_CONFIG["parameter_lists"][
                             "params_optimize_mixed_log_linear"
                         ][name + "_range_start"]
@@ -73,17 +73,18 @@ for folder in data_dir.iterdir():
                         end_value = SIM_CONFIG["parameter_lists"][
                             "params_optimize_mixed_log_linear"
                         ][name + "_range_end"]
-                        choices = np.round(np.arange(1, 20.1, 0.1), 1)
+
+                        choices = np.round(np.arange(start_value, end_value, 0.1), 1)
 
                         # Randomly select one value
                         random_value = np.random.choice(choices)
 
-                        x0.append(random_value)
+                        x0_MLLM.append(random_value)
 
                     # bounds of MLLM
-                    bounds = []
-                    for name in model_param_names:
-                        bounds.append(
+                    bounds_MLLM = []
+                    for name in MLLM_model_param_names:
+                        bounds_MLLM.append(
                             (
                                 SIM_CONFIG["parameter_lists"][
                                     "params_optimize_mixed_log_linear"
@@ -97,20 +98,21 @@ for folder in data_dir.iterdir():
                     # Optimized MLLM
                     optimized_mixed_log_linear = minimize(
                         fun=psy_funcs.mixed_log_linear_likelihood_scipy_minimize,
-                        x0=x0,
-                        bounds=bounds,
+                        x0=x0_MLLM,
+                        bounds=bounds_MLLM,
                         args=(np.array(given_numbers), np.array(estimates)),
                         method="L-BFGS-B",
                     )
 
                     # Optimize 1CPM
                     # 1CPM params
-                    model_param_names = list(
+                    oneCPM_model_param_names = list(
                         SIM_CONFIG["parameter_lists"]["params_one_cyclic_power"].keys()
                     )
+
                     # x0 of 1CPM
-                    x0 = []
-                    for name in model_param_names:
+                    x0_oneCPM = []
+                    for name in oneCPM_model_param_names:
                         start_value = SIM_CONFIG["parameter_lists"][
                             "params_optimize_one_cyclic_power"
                         ][name + "_range_start"]
@@ -118,17 +120,17 @@ for folder in data_dir.iterdir():
                         end_value = SIM_CONFIG["parameter_lists"][
                             "params_optimize_one_cyclic_power"
                         ][name + "_range_end"]
-                        choices = np.round(np.arange(1, 20.1, 0.1), 1)
+                        choices = np.round(np.arange(start_value, end_value, 0.1), 1)
 
                         # Randomly select one value
                         random_value = np.random.choice(choices)
 
-                        x0.append(random_value)
+                        x0_oneCPM.append(random_value)
 
                     # bounds of 1CPM
-                    bounds = []
-                    for name in model_param_names:
-                        bounds.append(
+                    bounds_oneCPM = []
+                    for name in oneCPM_model_param_names:
+                        bounds_oneCPM.append(
                             (
                                 SIM_CONFIG["parameter_lists"][
                                     "params_optimize_one_cyclic_power"
@@ -142,8 +144,55 @@ for folder in data_dir.iterdir():
                     # Optimized 1CPM
                     optimized_one_cyclic_power = minimize(
                         fun=psy_funcs.one_cyclic_power_likelihood_scipy_minimize,
-                        x0=x0,
-                        bounds=bounds,
+                        x0=x0_oneCPM,
+                        bounds=bounds_oneCPM,
+                        args=(np.array(given_numbers), np.array(estimates)),
+                        method="L-BFGS-B",
+                    )
+
+                    # Optimize 2CPM
+                    # 2CPM params
+                    twoCPM_model_param_names = list(
+                        SIM_CONFIG["parameter_lists"]["params_two_cyclic_power"].keys()
+                    )
+
+                    # x0 of 2CPM
+                    x0_twoCPM = []
+                    for name in twoCPM_model_param_names:
+                        start_value = SIM_CONFIG["parameter_lists"][
+                            "params_optimize_two_cyclic_power"
+                        ][name + "_range_start"]
+
+                        end_value = SIM_CONFIG["parameter_lists"][
+                            "params_optimize_two_cyclic_power"
+                        ][name + "_range_end"]
+
+                        choices = np.round(np.arange(start_value, end_value, 0.1), 1)
+
+                        # Randomly select one value
+                        random_value = np.random.choice(choices)
+
+                        x0_twoCPM.append(random_value)
+
+                    # Bounds of 2CPM
+                    bounds_twoCPM = []
+                    for name in twoCPM_model_param_names:
+                        bounds_twoCPM.append(
+                            (
+                                SIM_CONFIG["parameter_lists"][
+                                    "params_optimize_two_cyclic_power"
+                                ][name + "_range_start"],
+                                SIM_CONFIG["parameter_lists"][
+                                    "params_optimize_two_cyclic_power"
+                                ][name + "_range_end"],
+                            )
+                        )
+
+                    # Optimized 2CPM
+                    optimized_two_cyclic_power = minimize(
+                        fun=psy_funcs.two_cyclic_power_likelihood_scipy_minimize,
+                        x0=x0_twoCPM,
+                        bounds=bounds_twoCPM,
                         args=(np.array(given_numbers), np.array(estimates)),
                         method="L-BFGS-B",
                     )
@@ -152,24 +201,74 @@ for folder in data_dir.iterdir():
 
                     mllm_func = psy_funcs.mixed_log_linear
                     one_cpm_func = psy_funcs.one_cyclic_power
-
+                    two_cpm_func = psy_funcs.two_cyclic_power
                     mllm_params_opt = optimized_mixed_log_linear.x
                     one_cpm_params_opt = optimized_one_cyclic_power.x
+                    two_cpm_params_opt = optimized_two_cyclic_power.x
 
                     mllm_params_for_curve = mllm_params_opt[:-1]
                     one_cpm_params_for_curve = one_cpm_params_opt[:-1]
+                    two_cpm_params_for_curve = two_cpm_params_opt[:-1]
 
                     y_mllm = mllm_func(*mllm_params_for_curve, given_number=x_grid)
                     y_onecpm = one_cpm_func(
                         *one_cpm_params_for_curve, given_number=x_grid
                     )
+                    y_twocpm = [
+                        two_cpm_func(*two_cpm_params_for_curve, given_number=x_value)
+                        for x_value in x_grid
+                    ]
+
+                    MLLM_fitted_params_str = ""
+                    for i in range(len(MLLM_model_param_names)):
+                        param_name = MLLM_model_param_names[i][6:]
+                        optimized_value = round(mllm_params_opt[i], 2)
+
+                        if i == len(MLLM_model_param_names) - 1:
+                            MLLM_fitted_params_str += f"{param_name}: {optimized_value}"
+
+                        else:
+                            MLLM_fitted_params_str += (
+                                f"{param_name}: {optimized_value}, "
+                            )
+
+                    oneCPM_fitted_params_str = ""
+                    for i in range(len(oneCPM_model_param_names)):
+                        param_name = oneCPM_model_param_names[i][6:]
+                        optimized_value = round(one_cpm_params_opt[i], 2)
+                        if i == len(oneCPM_model_param_names) - 1:
+                            oneCPM_fitted_params_str += (
+                                f"{param_name}: {optimized_value}"
+                            )
+                        else:
+                            oneCPM_fitted_params_str += (
+                                f"{param_name}: {optimized_value}, "
+                            )
+
+                    twoCPM_fitted_params_str = ""
+                    for i in range(len(twoCPM_model_param_names)):
+                        param_name = twoCPM_model_param_names[i][6:]
+                        optimized_value = round(two_cpm_params_opt[i], 2)
+                        if i == len(twoCPM_model_param_names) - 1:
+                            twoCPM_fitted_params_str += (
+                                f"{param_name}: {optimized_value}"
+                            )
+                        else:
+                            twoCPM_fitted_params_str += (
+                                f"{param_name}: {optimized_value}, "
+                            )
 
                     plt.figure(figsize=(7, 5))
                     plt.scatter(
                         given_numbers, estimates, alpha=0.4, label="Data points"
                     )
-                    plt.plot(x_grid, y_mllm, label="MLLM (fitted)")
-                    plt.plot(x_grid, y_onecpm, label="1CPM (fitted)")
+                    plt.plot(x_grid, y_mllm, label=f"MLLM ({MLLM_fitted_params_str})")
+                    plt.plot(
+                        x_grid, y_onecpm, label=f"1CPM ({oneCPM_fitted_params_str})"
+                    )
+                    plt.plot(
+                        x_grid, y_twocpm, label=f"2CPM ({twoCPM_fitted_params_str})"
+                    )
                     plt.title(file.name)
                     plt.xlabel("Given Number")
                     plt.ylabel("Estimate")
@@ -188,6 +287,9 @@ for folder in data_dir.iterdir():
                     bic_1cpm = compute_bic(
                         optimized_one_cyclic_power, len(given_numbers)
                     )
+                    bic_2cpm = compute_bic(
+                        optimized_two_cyclic_power, len(given_numbers)
+                    )
 
                     bic_diff = bic_mllm - bic_1cpm
 
@@ -199,7 +301,9 @@ for folder in data_dir.iterdir():
                         bic_diff_bd.append(bic_diff)
 
                     plt.figure(figsize=(6, 4))
-                    bars = plt.bar(["MLLM", "1CPM"], [bic_mllm, bic_1cpm])
+                    bars = plt.bar(
+                        ["MLLM", "1CPM", "2CPM"], [bic_mllm, bic_1cpm, bic_2cpm]
+                    )
                     plt.ylabel("BIC")
                     title = f"BIC comparison - {file.name}"
                     plt.title(title)
@@ -234,7 +338,7 @@ plt.bar(x + bar_width, bic_diff_bd, width=bar_width, label="BD")
 
 plt.xlabel("Participant")
 plt.ylabel("BIC Difference")
-plt.title("BIC differences by optimization across participants (MLLM-1CPM)")
+plt.title("BIC differences by optimization across participants")
 plt.xticks(ticks=x, labels=participants_codes, rotation=45)
 ymin, ymax = plt.ylim()
 
@@ -253,4 +357,7 @@ title = "BIC differences by model across participants"
 plt.savefig(fname=f"plots/{title}.png")
 plt.show()
 
+# %%
+
+print("abc")
 # %%
--- simulation_NLE/psychometric_functions.py
+++ simulation_NLE/psychometric_functions.py
@@ -170,3 +170,94 @@ def one_cyclic_power_generate_response(param_exponent, param_noise, given_number
     response = np.random.normal(loc=pred, scale=param_noise)
     response = np.clip(response, 0, N_MAX)
     return response
+
+
+# def two_cyclic_power(param_exponent_left, param_exponent_right, given_number):
+#     number = np.power(given_number, param_exponent_left)
+#     denominator = number + np.power(N_MAX - given_number, param_exponent_right)
+#     return N_MAX * (number / denominator)
+
+
+# def two_cyclic_power(param_exponent_left, param_exponent_right, given_number):
+#     x = np.asarray(given_number, dtype=float)
+#     half = N_MAX / 2.0
+
+#     y = np.empty_like(x, dtype=float)
+
+#     # Left branch (x ≤ U/2)
+#     left = x <= half
+#     xl = x[left]
+#     num_l = xl**param_exponent_left
+#     den_l = num_l + (half - xl) ** param_exponent_left
+#     y[left] = half * (num_l / den_l)
+
+#     # Right branch (x > U/2)
+#     right = ~left
+#     xr = x[right]
+#     num_r = (xr - half) ** param_exponent_right
+#     den_r = num_r + (N_MAX - xr) ** param_exponent_right
+#     y[right] = half + half * (num_r / den_r)
+
+#     if np.isscalar(given_number):
+#         return float(y)
+#     return y
+
+
+def two_cyclic_power(param_exponent, given_number):
+    half = N_MAX / 2
+    if given_number <= half:
+        return half * (
+            given_number**param_exponent
+            / (given_number**param_exponent + (half - given_number) ** param_exponent)
+        )
+    else:
+        return half + half * (
+            (given_number - half) ** param_exponent
+            / (
+                (given_number - half) ** param_exponent
+                + (N_MAX - given_number) ** param_exponent
+            )
+        )
+
+
+def two_cyclic_power_likelihood(
+    param_exponent,
+    param_noise,
+    given_number,
+    simulated_estimate,
+):
+    pred = two_cyclic_power(param_exponent, given_number)
+
+    L = (
+        np.log(param_noise)
+        + 0.5 * np.log(2 * np.pi)
+        + ((simulated_estimate - pred) ** 2) / (2 * (param_noise**2))
+    )
+    return -L
+
+
+def two_cyclic_power_likelihood_scipy_minimize(
+    parameters, given_numbers, simulated_estimate
+):
+    param_exponent, param_noise = parameters
+
+    pred = np.array(
+        [
+            two_cyclic_power(param_exponent, given_number)
+            for given_number in given_numbers
+        ]
+    )
+
+    L = np.sum(
+        np.log(param_noise)
+        + 0.5 * np.log(2 * np.pi)
+        + ((simulated_estimate - pred) ** 2) / (2 * (param_noise**2))
+    )
+
+    return L
+
+
+def two_cyclic_power_generate_response(param_exponent, param_noise, given_number):
+    pred = two_cyclic_power(param_exponent, given_number)
+    response = np.random.normal(loc=pred, scale=param_noise)
+    return np.clip(response, 0, N_MAX)
